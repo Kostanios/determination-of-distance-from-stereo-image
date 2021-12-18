6ad2b3330cab3d8fa2f682f99cacd07e0c831630
Kotov Konstantin <kotov.const@mail.ru>
2021-12-18 13:29:55 +0300

    fix: calculate function

--- calculateDistance.py
+++ calculateDistance.py
@@ -1,2 +1,6 @@
-def calculate_distance(camera_delta, delta, f, matrix_size):
-    print(camera_delta, ' ', delta, ' ', f, ' ', matrix_size)
+# pixel_size = 2.834645
+
+
+def calculate_distance(camera_delta, delta, F, matrix_size, pixel_size):
+    print([camera_delta], ' ', [delta], ' ', [F], ' ', matrix_size)
+    return float(camera_delta) * float(F) / (float(pixel_size) * float(delta))
