d27ec32da03d09d7ed2a02408d6e384c866019d6
Kotov Konstantin <kotov.const@mail.ru>
2022-04-10 15:24:41 +0300

    fix: cycle error

--- SSD.py
+++ SSD.py
@@ -1,18 +1,22 @@
 def SSD(first_image_part, second_image_part):
-    x = 0
-    y = 0
+    x_size = len(first_image_part)
+    y_size = len(first_image_part[0])
     sum_of_dif_r = 0
     sum_of_dif_g = 0
     sum_of_dif_b = 0
-    while x < len(first_image_part):
-        while y < len(first_image_part[0]):
+    sum_of_dif_i = 0
+    x = 0
+    while x < x_size:
+        y = 0
+        while y < y_size:
             first_image_pixel = first_image_part[x][y]
             second_image_pixel = second_image_part[x][y]
 
-            sum_of_dif_r = sum_of_dif_r + pow(first_image_pixel[0] - second_image_pixel[0], 2)
-            sum_of_dif_g = sum_of_dif_g + pow(first_image_pixel[1] - second_image_pixel[1], 2)
-            sum_of_dif_b = sum_of_dif_b + pow(first_image_pixel[2] - second_image_pixel[2], 2)
+            sum_of_dif_r = sum_of_dif_r + pow(int(first_image_pixel[0]) - int(second_image_pixel[0]), 2)
+            sum_of_dif_g = sum_of_dif_g + pow(int(first_image_pixel[1]) - int(second_image_pixel[1]), 2)
+            sum_of_dif_b = sum_of_dif_b + pow(int(first_image_pixel[2]) - int(second_image_pixel[2]), 2)
+            sum_of_dif_i = sum_of_dif_i + pow(((int(first_image_pixel[0]) + int(first_image_pixel[1]) + int(first_image_pixel[2])) / 3) - ((int(second_image_pixel[0]) + int(second_image_pixel[1]) + int(second_image_pixel[2])) / 3), 2)
 
             y += 1
         x += 1
-    return [sum_of_dif_r, sum_of_dif_g, sum_of_dif_b]
+    return [sum_of_dif_r, sum_of_dif_g, sum_of_dif_b, sum_of_dif_i]
--- getDelta.py
+++ getDelta.py
@@ -5,6 +5,8 @@ import numpy as np
 plt.style.use('classic')
 
 def get_delta(point1, point2, main_image, second_image):
+    print(len(main_image))
+    print(len(main_image[0]))
     fig1, plts1 = plt.subplots()
     plts1.set_title('gra ssd')
     delta = 0
@@ -12,29 +14,35 @@ def get_delta(point1, point2, main_image, second_image):
     r_ssd = []
     g_ssd = []
     b_ssd = []
+    i_ssd = []
     min_ssd = SSD(main_image[point1.y():point2.y(), point1.x():point2.x()],
                   second_image[point1.y():point2.y(), point1.x():point2.x()])
-    max_offset = len(main_image[0]) - point2.x()
+    max_offset = 50
+    # max_offset = len(main_image[0]) - point2.x()
     print(max_offset, '-max offset')
     while x_offset < max_offset:
         new_ssd = SSD(main_image[point1.y():point2.y(), point1.x():point2.x()],
-                      second_image[point1.y():point2.y(), point1.x() + x_offset:point2.x() + x_offset])
+                      second_image[point1.y():point2.y(), (point1.x() + x_offset):(point2.x() + x_offset)])
+
         r_ssd.append(new_ssd[0])
         g_ssd.append(new_ssd[1])
         b_ssd.append(new_ssd[2])
-        print(x_offset)
-        if (min_ssd[0] + min_ssd[1] + min_ssd[2]) / 3 > (new_ssd[0] + new_ssd[1] + new_ssd[2]) / 3:
+        i_ssd.append(new_ssd[3])
+
+        if min_ssd[3] > new_ssd[3]:
             delta = x_offset
             min_ssd = new_ssd
         x_offset += 1
     delta_range = range(0, max_offset)
-    print(len(delta_range), '-', len(r_ssd))
     plts1.plot(np.array(delta_range), np.array(r_ssd), label='red spectral', color='r')
     plts1.plot(np.array(delta_range), np.array(g_ssd), label='green spectral', color='g')
     plts1.plot(np.array(delta_range), np.array(b_ssd), label='blue spectral', color='b')
+    plts1.plot(np.array(delta_range), np.array(i_ssd), label='all spectral', color='grey')
+
     plts1.legend()
     plt.xlabel('delta px')
     plt.xlabel(SSD)
+    # plt.imshow(second_image[point1.y():point2.y(), (point1.x() + delta):(point2.x() + delta)], interpolation='nearest')
     plt.show()
     print('------------------------------------------delta')
     print(delta)
